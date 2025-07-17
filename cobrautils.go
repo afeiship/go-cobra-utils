4a8fd9db7b1729490801143186954af391c02eec
aric.zheng <1290657123@qq.com>
2025-07-17 22:45:15 +0800

    feat: sh -> bash

--- cobrautils.go
+++ cobrautils.go
@@ -6,7 +6,7 @@ import (
 
 // Exec executes a shell command.
 func Exec(cmd string) (string, error) {
-	c := exec.Command("sh", "-c", cmd)
+	c := exec.Command("bash", "-c", cmd)
 	output, err := c.CombinedOutput()
 	return string(output), err
 }
